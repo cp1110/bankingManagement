65b2273e2b2c7735d962509f1167f355df57961c
CP <cpsagar55511@gmail.com>
2023-08-05 01:38:19 +0530

    my first commit

---
+++ code.py
@@ -0,0 +1,5 @@
+print("Hello World");
+print(2+2);
+user = 2;
+print(type(user));
+
---
+++ temp.sql
@@ -0,0 +1,9 @@
+
+
+create TABLE student{
+    name: varchar(50),
+    RegNo: INT PRIMARY,
+    girlFriend: varchar(10)
+}
+
+SELECT * TABLE from student;
